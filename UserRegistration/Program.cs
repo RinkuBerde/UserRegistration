db2b451e8f1355b9ce63853f65a7bbeeb76ede24
rinkuberde <rinkuberde@gmail.com>
2023-01-24 15:17:23 +0530

    [Rinku] - valid First Name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace UserRegistration
+﻿using System;
+
+namespace UserRegistration
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Practice Problems!!!");
+            Console.WriteLine("\n1. Enter valid First Name");
+            Console.Write("Enter your choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Validate validate1 = new Validate();
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter Your First Name : ");
+                    string FirstName = Console.ReadLine();
+                    Console.WriteLine(validate1.ValidateFirstName(FirstName));
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter correct choice");
+                    break;
+
+            }
         }
     }
 }
---
+++ UserRegistration/Validate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UserRegistration
+{
+    internal class Validate
+    {       
+        //Regular expression or validating first name
+       public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";          
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }        
+    }
+}
+
