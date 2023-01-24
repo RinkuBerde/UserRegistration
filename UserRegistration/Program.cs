5917822a3b7a95e6f654418aa13bde4184271596
rinkuberde <rinkuberde@gmail.com>
2023-01-24 16:00:21 +0530

    [Rinku] - Valid Email

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -9,6 +9,7 @@ namespace UserRegistration
             Console.WriteLine("Welcome to User Registration Practice Problems!!!");
             Console.WriteLine("\n1. Enter valid First Name");
             Console.WriteLine("2. Enter valid Last Name");
+            Console.WriteLine("3. Enter valid email");
             Console.Write("Enter your choice : ");
             int choice = Convert.ToInt32(Console.ReadLine());
             Validate validate1 = new Validate();
@@ -23,6 +24,11 @@ namespace UserRegistration
                     Console.Write("Enter Your Last Name : ");
                     string LastName = Console.ReadLine();
                     Console.WriteLine(validate1.ValidateLastName(LastName));
+                    break;
+                case 3: 
+                    Console.Write("Enter Your Email : ");
+                    string email = Console.ReadLine();
+                    Console.WriteLine(validate1.ValidateEmail(email));
                     break;
                 default:
                     Console.WriteLine("Please enter correct choice");
--- UserRegistration/Validate.cs
+++ UserRegistration/Validate.cs
@@ -14,6 +14,8 @@ namespace UserRegistration
        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
         // Regular expression for last name
         public static string Regex_LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        //Regular expression for valid email
+        public const string Regex_Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
@@ -21,6 +23,10 @@ namespace UserRegistration
         public bool ValidateLastName(string LastName)
         {
             return Regex.IsMatch(LastName, Regex_LastName);
+        }
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, Regex_Email);
         }
     }
 }
