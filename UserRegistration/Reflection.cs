0188799c38dd26e1030111f5459c52cc9d5a1f1d
rinkuberde <rinkuberde@gmail.com>
2023-02-01 19:12:16 +0530

    [Rinku] - perform Reflection

---
+++ UserRegistration/Reflection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Reflection
+    {
+            public object CreateUserRegistration(string className, string constructorName)
+            {
+                string pattern = @"." + constructorName + "$";
+                Match result = Regex.Match(className, pattern);
+
+                if (result.Success)
+                {
+                    try
+                    {
+                        Assembly executing = Assembly.GetExecutingAssembly();
+                        Type userRegistrationType = executing.GetType(className);
+                        return Activator.CreateInstance(userRegistrationType);
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        throw new ValidationExceptions(ValidationExceptions.ExceptionType.NO_SUCH_CLASS, "Class not found");
+                    }
+                }
+                else
+                {
+                    throw new ValidationExceptions(ValidationExceptions.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
+                }
+            }
+            public object CreateUserRegistrationUsingParameterizedConstructor(string className, string constructorName, string message)
+            {
+                Type type = typeof(Validate);
+                if (type.Name.Equals(className) || type.FullName.Equals(className))
+                {
+                    try
+                    {
+                        ConstructorInfo constructorInfo = type.GetConstructor(new[] { typeof(string) });
+                        object instance = constructorInfo.Invoke(new object[] { message });
+                        return instance;
+                    }
+                    catch (ArgumentNullException)
+                {
+                        throw new ValidationExceptions(ValidationExceptions.ExceptionType.NO_SUCH_CLASS, "Class not found");
+                    }
+                }
+                else
+                {
+                    throw new ValidationExceptions(ValidationExceptions.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
+                }
+            }
+        }
+}
--- UserRegistration/ValidationExceptions.cs
+++ UserRegistration/ValidationExceptions.cs
@@ -7,17 +7,16 @@ using System.Threading.Tasks;
 namespace UserRegistration
 {
     public class ValidationExceptions:Exception
-    {
-        
+    {       
         public enum ExceptionType
         {
             INVALID_FIRST_NAME,
             INVALID_LAST_NAME,
             INVALID_EMAIL,
             INVALID_MOBILE_NUMBER,
-            INVALID_PASSWORD
+            INVALID_PASSWORD,
+            NO_SUCH_CLASS, NO_SUCH_METHOD
         }
-
         public ExceptionType type;
         public ValidationExceptions(ExceptionType type, string message) : base(message)
         {
